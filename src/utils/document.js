fd2d9196873fea656322549ce53fdee1730d1649
falsaffa <44938179+falsaffa@users.noreply.github.com>
2020-02-11 09:50:31 -0600

    Fix Pendo 404 Integration (#1357)
    Co-authored-by: Kristen Surratt <51092436+ksatx@users.noreply.github.com>

--- src/utils/document.js
+++ src/utils/document.js
@@ -62,17 +62,17 @@ export function Document({ Html, Head, Body, children, state }) {
         <meta name="twitter:image" content={SITE_ROOT + meta.twitter.image} />
         <link rel="shortcut icon" href={meta.favicon} type="image/x-icon" />
         {meta.canonical && <link rel="canonical" href={meta.canonical} />}
-        {PENDO_API_KEY && (
+        {PENDO_API_KEY != null && (
           <script
             type="text/javascript"
             dangerouslySetInnerHTML={{
-              __html: (function(apiKey){
+              __html: `(function(apiKey){
               (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
               v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){
                   o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
                   y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
                   z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-          })(PENDO_API_KEY),
+          })('${PENDO_API_KEY}')`,
             }}
           />
         )}
